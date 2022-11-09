40c4325a3dca484e4b6a877f7518bd6100d3543b
RobsonOliveira1986 <robson19ps2@gmail.com>
2022-11-09 16:53:31 -0300

    alteração em ambiente

--- Meu primeiro projeto Java/src/App.java
+++ Meu primeiro projeto Java/src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Salve Time DIO");
+        System.out.println(); {
+            int l1 = 10;
+            int l2 = 20;
+            float f1 = 4.5f;
+            float f2 = 3.5f;
+            double d1 = 59.6d;
+            char c1 = 'x';
+            char c2 = 'y';
+            String s1 = "Fulano";
+            String s2 = "Fulano";
+            String s3 = "Cicrano";
+            boolean b1 = true;
+            boolean b2 = false;
+
+            System.out.println("i1 == i2" + (l1 == l2));
+            System.out.println("i1 != i2" + (l1 != l2));
+
+
+
+        }
+
     }
 }
