648272a18e179911572a94b6cefe9c891a1d9f79
RobsonOliveira1986 <robson19ps2@gmail.com>
2022-11-09 21:01:43 -0300

    aprendendo metodos

---
+++ metodos/src/Usuario.java
@@ -0,0 +1,11 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+    SmartTv smartTv = new SmartTv();
+
+    System.out.print("TV ligada?" + smartTv.ligada);
+    System.out.print("canal atual?" + smartTv.canal);
+    System.out.print("volume atual?" + smartTv.volume);
+
+  
+    }
+}
